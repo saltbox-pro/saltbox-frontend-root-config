f0fe605b7c2334d7d1fc9b7194b3b0d5038f0434
Oleg Koveshnikov <o.koveshnikov@saltbox.pro>
2025-07-31 14:53:15 +0300

    fix: update example dev config

--- example.config.dev.ts
+++ example.config.dev.ts
@@ -8,16 +8,12 @@ export const saltboxMainConfig = {
         "client_id": "saltbox_core",
         "redirect_uri": "http://localhost:4200",
         "client_secret": "PWldvmaA9IW1tHLP",
-        "keycloak_oidc_url": "http://localhost/auth/keycloak/realms/salt.box/.well-known/openid-configuration",
-        "keycloak_authorization_endpoint": "http://localhost/auth/keycloak/realms/salt.box/protocol/openid-connect/auth"
     },
     "services": [{
         "url": "http://localhost:4202",
-        "static_host": "http://localhost:4202",
         "env": {
             "api_base_path": "http://localhost/api/core",
-            "ws_server_url": null
+            "ws_server_url": "ws://localhost/api/core"
         }
     }],
-    "keycloak_url": "http://localhost/auth/keycloak"
 }
