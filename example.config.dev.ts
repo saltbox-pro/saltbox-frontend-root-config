3edcb86ec05bccc087460afa883f10687b6064ac
Oleg Koveshnikov <o.koveshnikov@saltbox.pro>
2025-09-16 15:25:15 +0300

    fix: update example config

--- example.config.dev.ts
+++ example.config.dev.ts
@@ -3,17 +3,34 @@ export const saltboxBaseUrl = "http://localhost:4201/index.js";
 export const saltboxDiscoveryUrl = "/api/discovery/config";
 
 export const saltboxMainConfig = {
-    "auth_config": {
-        "authority": "http://localhost/auth/keycloak/realms/salt.box",
-        "client_id": "saltbox_core",
-        "redirect_uri": "http://localhost:4200",
-        "client_secret": "PWldvmaA9IW1tHLP",
-    },
-    "services": [{
-        "url": "http://localhost:4202",
-        "env": {
-            "api_base_path": "http://localhost/api/core",
-            "ws_server_url": "ws://localhost/api/core"
-        }
-    }],
+  "auth_config": {
+    "authority": "http://localhost/auth/keycloak/realms/salt.box",
+    "client_id": "saltbox_core",
+    "redirect_uri": "http://localhost:4200",
+    "client_secret": "PWldvmaA9IW1tHLP",
+  },
+  "services": [{
+    "service_name": "core",
+    "url": "http://localhost:4202",
+    "env": {
+      "api_base_path": "http://localhost/api/core",
+      "ws_server_url": "ws://localhost/api/core"
+    }
+  },
+  {
+    "service_name": "scheduler",
+    "url": "http://localhost:4204",
+    "env": {
+      "api_base_path": "http://localhost/api/scheduler",
+      "ws_server_url": null
+    }
+  },
+  {
+    "service_name": "inventory",
+    "url": "http://localhost:4205",
+    "env": {
+      "api_base_path": "http://localhost/api/inventory",
+      "ws_server_url": null
+    }
+  },],
 }
