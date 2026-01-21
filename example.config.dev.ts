e646b1eca779b641be5db7afe72398b936a7c3c8
Oleg Koveshnikov <o.koveshnikov@saltbox.pro>
2026-01-21 14:39:36 +0300

    fix: update example config for dev

--- example.config.dev.ts
+++ example.config.dev.ts
@@ -3,43 +3,52 @@ export const saltboxBaseUrl = "http://localhost:4201/index.js";
 export const saltboxDiscoveryUrl = "/api/discovery/config";
 
 export const saltboxMainConfig = {
-  "auth_config": {
-    "authority": "https://localhost/auth/keycloak/realms/salt.box",
-    "client_id": "saltbox_core",
-    "redirect_uri": "http://localhost:4200",
-    "client_secret": "PWldvmaA9IW1tHLP",
+  auth_config: {
+    authority: "https://localhost/auth/keycloak/realms/salt.box",
+    client_id: "saltbox_core",
+    redirect_uri: "http://localhost:4200",
+    client_secret: "PWldvmaA9IW1tHLP",
   },
-  "services": [{
-    "service_name": "core",
-    "url": "http://localhost:4202",
-    "env": {
-      "api_base_path": "https://localhost/api/core",
-      "ws_server_url": "wss://localhost/api/core"
-    }
-  },
-  {
-    "service_name": "scheduler",
-    "url": "http://localhost:4204",
-    "env": {
-      "api_base_path": "https://localhost/api/scheduler",
-      "ws_server_url": null
-    }
-  },
-  {
-    "service_name": "inventory",
-    "url": "http://localhost:4205",
-    "env": {
-      "api_base_path": "https://localhost/api/inventory",
-      "ws_server_url": null
-    }
-  },
-  {
-    "service_name": "gateway",
-    "url": "http://localhost:4203",
-    "env": {
-      "api_base_path": "https://localhost",
-      "ws_server_url": null
-    }
-  },
+  services: [
+    {
+      service_name: "core",
+      url: "http://localhost:4202",
+      env: {
+        api_base_path: "https://localhost/api/core",
+        ws_server_url: "wss://localhost/api/core",
+      },
+    },
+    {
+      service_name: "scheduler",
+      url: "http://localhost:4204",
+      env: {
+        api_base_path: "https://localhost/api/scheduler",
+        ws_server_url: null,
+      },
+    },
+    {
+      service_name: "inventory",
+      url: "http://localhost:4205",
+      env: {
+        api_base_path: "https://localhost/api/inventory",
+        ws_server_url: null,
+      },
+    },
+    {
+      service_name: "gateway",
+      url: "http://localhost:4203",
+      env: {
+        api_base_path: "https://localhost",
+        ws_server_url: null,
+      },
+    },
+    {
+      service_name: "migration",
+      url: "http://localhost:4207",
+      env: {
+        api_base_path: "https://192.168.1.222/api/migration",
+        ws_server_url: "wss://192.168.1.222/api/migration",
+      },
+    },
   ],
-}
+};
